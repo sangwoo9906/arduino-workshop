b753ce7df427c703a2a992e19a20f78fa9665154
sangwoo9906 <56580211+sangwoo9906@users.noreply.github.com>
2019-10-15 18:01:08 +0900

    Update 01.digital_light.ino

--- basic/01.digital_light/01.digital_light.ino
+++ basic/01.digital_light/01.digital_light.ino
@@ -1,11 +1,11 @@
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);                       
-  digitalWrite(LED_BUILTIN, LOW);   
+  digitalWrite(2, HIGH);  
+  delay(100);                       
+  digitalWrite(2, LOW);   
   delay(100);                      
 }
