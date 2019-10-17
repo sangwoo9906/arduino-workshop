dff6f2a457ed001d24560a9d459edc43185583c3
sangwoo9906 <56580211+sangwoo9906@users.noreply.github.com>
2019-10-17 16:44:10 +0900

    Update new2.ino

--- advanced/new2/new2.ino
+++ advanced/new2/new2.ino
@@ -1,10 +1,15 @@
+int R =0;
+int X =0;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(3,OUTPUT);
 }
 
+
 void loop() {
-  if(Serial.available()){
-    long value = Serial.parseInt();
-    Serial.println(value);
-  }
+  R = analogRead(A0);
+  R = map(R, 0, 1023,0, 255);
+  analogWrite(3,R);
+  
 }
